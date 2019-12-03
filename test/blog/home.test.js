2caf1a78dabda5e84605fb58bc8c1ef282889732
lml <soalin228@163.com>
2019-12-03 11:17:26 +0800

    test: 添加测试用户信息 加载微博测试

--- test/blog/home.test.js
+++ test/blog/home.test.js
@@ -5,39 +5,10 @@
  */
 
 const server = require('../server')
+const { COOKIE } = require('../testUserInfo')
 
-// 用户信息
-const userName = `u_${Date.now()}`
-const password = `p_${Date.now()}`
-const testUser = {
-  userName,
-  password,
-  nickName: userName,
-  gender: 1
-}
-
-let COOKIE = ''
-
+// 存储微博 id
 let BLOG_ID = ''
-
-test('注册一个用户, 期望成功', async () => {
-  const res = await server
-    .post('/api/user/register')
-    .send(testUser)
-  expect(res.body.errno).toBe(0)
-})
-
-test('登录，期望成功', async () => {
-  const res = await server
-    .post('/api/user/login')
-    .send({
-      userName,
-      password
-    })
-  expect(res.body.errno).toBe(0)
-
-  COOKIE = res.headers['set-cookie'].join(';')
-})
 
 test('创建一条微博, 期望成功', async () => {
   const content = '单元测试自动创建的微博_' + Date.now()
@@ -55,18 +26,4 @@ test('创建一条微博, 期望成功', async () => {
   expect(res.body.data.image).toBe(image)
 
   BLOG_ID = res.body.data.id
-})
-
-test('删除用户，期望成功', async () => {
-  const res = await server
-    .post('/api/user/delete')
-    .set('cookie', COOKIE)
-  expect(res.body.errno).toBe(0)
-})
-
-test('退出登录，期望成功', async () => {
-  const res = await server
-    .post('/api/user/logout')
-    .set('cookie', COOKIE)
-  expect(res.body.errno).toBe(0)
 })
---
+++ test/testUserInfo.js
@@ -0,0 +1,9 @@
+/**
+ * @description 单元测试的用户信息
+ * @author SOALIN
+ * @date 2019/12/3 10:54
+ */
+module.exports = {
+  USER_NAME: 'sss',
+  COOKIE: 'weibo.sid=qgztnZYrIJIw9Xld3yJExncKwlmNCdy7; weibo.sid.sig=kia2vWb8Y-o38Ke5VwlFb86xVrc'
+}
