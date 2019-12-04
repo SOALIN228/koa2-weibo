ecd9f9cb4c6c6855e53f069497e708e1f1f7c4d5
lml <soalin228@163.com>
2019-12-04 22:22:05 +0800

    test: at API

--- test/user/relation.test.js
+++ test/user/relation.test.js
@@ -43,6 +43,18 @@ test('获取id_1的关注人，期望id_2存在', async () => {
   expect(hasUserName).toBe(true)
 })
 
+test('获取id_1的 at 列表，期望id_2存在', async () => {
+  const result = await server
+    .get('/api/user/getAtList')
+    .set('cookie', COOKIE_1)
+  const atList = result.body
+  const hasUserName = atList.some(item => {
+    // nickName - userName
+    return item.indexOf(`- ${USER_NAME_2}`) > 0
+  })
+  expect(hasUserName).toBe(true)
+})
+
 test('id_1取消关注id_2, 期望成功', async () => {
   const res = await server
     .post('/api/profile/unFollow')
