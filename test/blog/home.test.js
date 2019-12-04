83b44516df7f582b6c99ebb1adfaf78c4776cac7
lml <soalin228@163.com>
2019-12-04 21:02:05 +0800

    test: 首页微博

--- test/blog/home.test.js
+++ test/blog/home.test.js
@@ -27,3 +27,17 @@ test('创建一条微博, 期望成功', async () => {
 
   BLOG_ID = res.body.data.id
 })
+
+test('首页，加载第一页数据，期望成功', async () => {
+  const res = await server
+    .get(`/api/blog/loadMore/0`)
+    .set('cookie', COOKIE_1)
+  expect(res.body.errno).toBe(0)
+
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
+})
